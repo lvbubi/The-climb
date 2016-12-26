af7245208975631bef9a2c642becadb568bee7de
Bence Bubori <lvbubi2@gmail.com>
2016-12-26 14:33:54 +0100

    first

---
+++ Assets/BoxScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxScript : MonoBehaviour {
+    private Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = transform.GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        if (col.gameObject.name == "troll")
+        {
+            
+            Debug.Log("Collision");
+            
+
+            rigidbody.constraints &= ~RigidbodyConstraints.FreezePositionX;
+            rigidbody.constraints &= ~RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        if (col.gameObject.name == "troll")
+        {
+            Debug.Log("Collision");
+            rigidbody.constraints = RigidbodyConstraints.FreezeAll; 
+        }
+    }
+
+}
---
+++ Assets/controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+    public float speed = 2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnCollisionExit(Collision col)
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+        // Update is called once per frame
+    void Update () {
+        moove();
+    }
+
+
+    void moove()
+    {
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 offset = new Vector3(x, 0.0f, z) * speed;
+
+        transform.position += offset * Time.deltaTime;
+
+
+        GetComponent<Animator>().SetFloat(
+              "speed", offset.magnitude
+          );
+        if (x != 0 || z != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(offset.normalized);
+        }
+    }
+}

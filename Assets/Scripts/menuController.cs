64d30849fc5002c8c3022b3166a19732bf4f21a0
Andrika Balint <andrika.balint@gmail.com>
2016-12-31 02:19:37 +0100

    MAIN MENU

---
+++ Assets/Scripts/menuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour {
+
+	public Button Play;
+	public Button Exit;
+
+	// Use this for initialization
+	void Start () {
+	
+		Play = Play.GetComponent<Button> ();
+		Exit = Exit.GetComponent<Button> ();
+
+	}
+	
+	// Update is called once per frame
+	 void Update () {
+
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene ("main");
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}

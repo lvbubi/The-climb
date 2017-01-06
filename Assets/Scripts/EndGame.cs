e8ebecb246feb02b77de96c5020c8f31a917bf0a
Andrika Balint <andrika.balint@gmail.com>
2017-01-06 15:22:33 +0100

    Credis Update (retur to main menu)

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour {
+
+	void Start() {
+		StartCoroutine(Example());
+	}
+
+	IEnumerator Example() {
+		
+		yield return new WaitForSeconds(25);
+		SceneManager.LoadScene("MainMenu");
+	}
+
+}
